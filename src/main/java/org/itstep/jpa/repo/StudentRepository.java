5ae3fbd7063d452bf16bb876d9941d9769178f7d
David <sir.baimyrza@gmail.com>
2021-09-20 18:32:07 +0600

    Created methods in repository for some sql results

--- src/main/java/org/itstep/jpa/repo/StudentRepository.java
+++ src/main/java/org/itstep/jpa/repo/StudentRepository.java
@@ -3,8 +3,18 @@ package org.itstep.jpa.repo;
 import org.itstep.jpa.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    List<Student> findAllByAgeGreaterThanEqual(Integer value);
+
+    List<Student> findAllByAgeLessThanEqual(Integer value);
+
+    List<Student> findAllByNameContaining(String value);
+
+    List<Student> findAllByNameLike(String value);
+
 
 }
